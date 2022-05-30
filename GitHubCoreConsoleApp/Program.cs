c478b22ef23808479162f734c3d0aad0f2de962b
tejaswinivm <tejutejaswini63@gmail.com>
2022-05-30 12:21:17 +0530

    new line added

--- GitHubCoreConsoleApp/Program.cs
+++ GitHubCoreConsoleApp/Program.cs
@@ -7,6 +7,12 @@ namespace GitHubCoreConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hello ma'am");
+            Add(1, 2);
+        }
+        static void Main(int n1,int n2)
+        {
+            Console.WriteLine("sum of {0}", n1 + n2); ;
         }
     }
 }
