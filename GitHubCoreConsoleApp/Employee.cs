018860cbb846fd4280327c41c39a544392a02fe9
Tejaswini-31 <106510836+Tejaswini-31@users.noreply.github.com>
2022-05-30 12:32:27 +0530

    Create Employee.cs

---
+++ GitHubCoreConsoleApp/Employee.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace GitHubCoreConsoleApp
+{
+    class Employee
+    {
+        public int EmpId{set; get;}
+        public string EmpName{ set; get;}
+        public double Salary{set;get;}
+        
+      }
+      
+  }
+  
+       
