5b8c46dc9b75aff65ff834a8e61939dd50340a90
Tejaswini-31 <106510836+Tejaswini-31@users.noreply.github.com>
2022-05-30 12:44:35 +0530

    Update Program.cs

--- GitHubCoreConsoleApp/Program.cs
+++ GitHubCoreConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace GitHubCoreConsoleApp
             Console.WriteLine("hello ma'am");
             Add(1, 2);
         }
-        static void Main(int n1,int n2)
+        static void Add(int n1,int n2)
         {
             Console.WriteLine("sum of {0}", n1 + n2); ;
         }
